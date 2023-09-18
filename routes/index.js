861db6017d0da3ba5c982618751ce2208ca6648e
c99-coder <c99@kakao.com>
2023-09-18 21:28:53 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.html');
+const app = express();
+
+// 변경된 부분: createProxyMiddleware 함수를 사용
+app.use(
+  "/proxy",
+  createProxyMiddleware({
+    target: "https://www.op.gg",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/proxy": "",
+    },
+  })
+);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = router;
